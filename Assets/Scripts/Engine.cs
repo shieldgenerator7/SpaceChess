eabdd8522d9d80c57d10492b94d443b09eca9e96
shieldgenerator7 <shieldgenerator7@yahoo.com>
2021-07-24 07:56:58 -0400

    Movement can be blocked by pieces

--- Assets/Scripts/Engine.cs
+++ Assets/Scripts/Engine.cs
@@ -28,6 +28,36 @@ public abstract class Engine
                 }
             }
         }
+        //Verify moves are not blocked
+        for (int r = 1; r <= range; r++)
+        {
+            for (int i = v.x - r; i <= v.x + r; i++)
+            {
+                for (int j = v.y - r; j <= v.y + r; j++)
+                {
+                    //Only look at positions in this ring
+                    if (Mathf.Abs(i - v.x) == r || Mathf.Abs(j - v.y) == r)
+                    {
+                        //Only look at positions that are valid
+                        if (moves[i, j] == true)
+                        {
+                            //If the cell directly "below" is invalid,
+                            if (moves[reduceInt(i), reduceInt(j)] == false)
+                            {
+                                //this cell is invalid
+                                moves[i, j] = false;
+                            }
+                            //If the cell is not empty
+                            else if (grid[i, j] != piece && grid[i, j] != null && grid[i, j].blocking)
+                            {
+                                //this cell is invalid
+                                moves[i, j] = false;
+                            }
+                        }
+                    }
+                }
+            }
+        }
         //Remove current cell
         moves[v.x, v.y] = false;
         //Return moves
@@ -39,4 +69,9 @@ public abstract class Engine
 
     protected PositionFunction Diagonal
         => (x, y, v) => Mathf.Abs(x - v.x) == Mathf.Abs(y - v.y);
+
+    private int reduceInt(int i)
+    {
+        return (i != 0) ? i - (int)Mathf.Sign(i) : 0;
+    }
 }
--- Assets/Scripts/Piece.cs
+++ Assets/Scripts/Piece.cs
@@ -7,4 +7,5 @@ public class Piece
     public Vector2Int position;
     public Vector2Int direction;
     public Engine engine;
+    public bool blocking = true;//true: pieces cannot move past this piece
 }
